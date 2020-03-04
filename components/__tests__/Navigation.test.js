43789cac99eeacbb92ebd3ccb45fe6383830177b
Nick Kramer <krames12@gmail.com>
2020-03-03 20:43:05 -0500

    adds jest-axe for a11y violations, works

--- components/__tests__/Navigation.test.js
+++ components/__tests__/Navigation.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-
 import { render, cleanup } from "@testing-library/react";
+const { axe } = require("jest-axe");
+
 import Navigation from "../Navigation";
 
 afterEach(cleanup);
@@ -9,4 +10,11 @@ test("Navigation should render", () => {
   const { getByText } = render(<Navigation />);
 
   expect(getByText("Tasky Task")).toBeInTheDocument();
+});
+
+test("navigation should not have any accessibility violations", async () => {
+  const { container } = render(<Navigation />);
+
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
 });
--- setupTests.js
+++ setupTests.js
@@ -4,3 +4,6 @@
 // used for __tests__/testing-library.js
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import { toHaveNoViolations } from "jest-axe";
+
+expect.extend(toHaveNoViolations);
